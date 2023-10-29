6b028e1bb982c3bccab36496409ccc13d2328c4e
anna-teruel <teruel.anna@gmail.com>
2023-10-29 21:59:45 +0100

    adding density_plot function to plotting.py

--- dlc/plotting.py
+++ dlc/plotting.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
+import seaborn as sns
 
-def plot_interpolation(ax, data, title):
+def interpolation_plot(ax, data, title):
     """
     Plot the provided data on a given axis, highlighting segments based on likelihood values.
 
@@ -36,3 +37,37 @@ def plot_interpolation(ax, data, title):
     ax.set_title(title)
 
     return lc
+
+def density_plot(bodypart, cmap='viridis', cbar=True, title:str=""):
+    """
+    Generate a 2D kernel density estimate (KDE) plot for the provided data points.
+
+    This function visualizes the density of data points in a 2D space. 
+    The plot uses color intensities to represent areas of higher vs. lower point density.
+    
+    Args:
+        bodypart (pandas.DataFrame): A dataframe containing columns 'x' and 'y', which represent 
+                               the coordinates of the data points to be plotted.
+        cmap (str, optional): Colormap to be used for the KDE plot. Defaults to 'viridis'.
+        cbar (bool, optional): Whether to display a colorbar in the plot to indicate the 
+                               density scale. Defaults to True.
+        title (str, optional): Title to set for the generated plot. Defaults to an empty string.
+
+    Returns:
+        None: The function saves the generated plot as a PDF file and does not return a value.
+    """       
+    plt.subplots(figsize=(10, 10))
+    sns.set(style="dark")
+    ax = sns.kdeplot(
+            bodypart["x"],
+            bodypart["y"],
+            shade=True,
+            fill=True,
+            cbar=cbar,
+            cmap=cmap,
+            levels=10,
+            alpha=0.9,
+        )
+    plt.title(title)
+    ax.set_aspect("equal", adjustable="box") 
+    plt.savefig("Density_plot_" + title + ".pdf", format="pdf", dpi=100)
