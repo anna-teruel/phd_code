6335d773ad6cde55dd1b2135215c10042f9d07fe
anna-teruel <teruel.anna@gmail.com>
2023-10-30 20:41:10 +0100

    update on some scripts for dlc analysis

--- dlc/__init__.py
+++ dlc/__init__.py
@@ -8,7 +8,6 @@ import dlc.data as data
 import dlc.plotting as plotting
 import dlc.load_data as load_data
 import dlc.analysis.kinematics as kinematics
-import dlc.analysis.time_roi as time_roin
-import dlc.minicam_data as minicam_data
+import dlc.analysis as analysis
 
 __all__ = ["data", "plotting", "load_data", "kinematics", "time_roi"]
