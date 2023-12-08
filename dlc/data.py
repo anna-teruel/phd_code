bee3596156836b656b3004164b70cb1e1946edcf
anna-teruel <teruel.anna@gmail.com>
2023-12-09 00:15:51 +0100

    improved load_data and data file. work in progress with plotting.py

--- dlc/data.py
+++ dlc/data.py
@@ -4,22 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.collections import LineCollection
 
+from dlc.load_data import DataLoader
 
+class Centroid:
+    def __init__(self, minutes=None, fps=None):
+        self.minutes = minutes
+        self.fps = fps
 
-def get_centroid(data_dict, bodyparts): 
-    #TODO: make get the centroid function available for unique files, not only for dicts
-    """
-    Calculate the centroid of specified bodyparts in each DataFrame in the dictionary.
+    def calculate_centroid(self, df, bodyparts):
+        """
+        Calculate the centroid for a given DataFrame.
 
-    Args:
-        data_dict (dict): A dictionary where keys are identifiers and values are DataFrames of bodypart coordinates.
-        bodyparts (list): A list of bodyparts to include in the centroid calculation.
+        Args:
+            df (pd.DataFrame): DataFrame containing bodypart coordinates.
+            bodyparts (list): List of bodyparts to include in the centroid calculation.
 
-    Returns:
-        centroids (dict): A dictionary with the same keys, where values are the centroids of the specified bodyparts.
-    """
-    centroids = {}
-    for key, df in data_dict.items():
+        Returns:
+            pd.DataFrame: DataFrame with added centroid coordinates.
+        """
         x_columns = [bp for bp in bodyparts]
         y_columns = [bp for bp in bodyparts]
 
@@ -28,40 +30,116 @@ def get_centroid(data_dict, bodyparts):
 
         DLCscorer = df.columns[0][0]
 
-        df.loc[:, (DLCscorer,"centroid", "x")] = centroid_x
+        df.loc[:, (DLCscorer, "centroid", "x")] = centroid_x
         df.loc[:, (DLCscorer, "centroid", "y")] = centroid_y
+        return df
 
-        centroids[key] = df
+    def get_centroid(self, input_data, bodyparts):
+        """
+        Get centroid for single or multiple DataFrames.
 
-    return centroids
+        Args:
+            input_data (str): Path to .h5 file/s.
+            bodyparts (list): List of bodyparts for centroid calculation.
+
+        Returns:
+            dict or pd.DataFrame: Dictionary with centroids or single DataFrame with centroid.
+        """
+        if isinstance(input_data, str):
+            if os.path.isfile(input_data):
+                # If it's a file; read the single file
+                loader = DataLoader(input_data, self.minutes, self.fps)
+                df = loader.read_data(input_data)
+                return self.calculate_centroid(df, bodyparts)
+            elif os.path.isdir(input_data):
+                # If it's a directory; read all files in the directory
+                loader = DataLoader(input_data, self.minutes, self.fps)
+                data_dict = loader.read_all_data()
+                return {key: self.calculate_centroid(df, bodyparts) for key, df in data_dict.items()}
+            else:
+                raise ValueError("Provided path does not exist.")
+        else:
+            raise ValueError("Input data must be a file path or directory path")
 
 
-def get_interpolation(data_dict, bodyparts, threshold=0.95, interpolation_method='linear'):
-    #TODO: make interpolation available for unique files, not only for dicts. 
-    """
-    Interpolate x,y coordinates for specified body parts in a DataFrame when their likelihood 
-    is below a given threshold.
 
-    Args:
-        data_dict (dict): A dictionary where keys are identifiers and values are DataFrames of bodypart coordinates.
-        bodyparts (list): A list of bodyparts to interpolate.
-        threshold (float): Likelihood threshold below which interpolation should be performed. Defaults to 0.95.
-        interpolation_method (string): Interpolation method used. Default linear interpolation.
-    
-    Returns:
-        dict: Dictionary with DataFrames that have interpolated values for the specified body parts.
-    """   
-    for key, df in data_dict.items():
+class Interpolation:
+    def __init__(self, threshold=0.90, interpolation_method='linear'):
+        """
+        Initialize an Interpolator object with specified threshold and interpolation method.
+
+        The Interpolator class is designed to interpolate x, y coordinates of specified body parts 
+        in a DataFrame or a collection of DataFrames. The interpolation is applied where the likelihood 
+        of body part coordinates is below a given threshold.
+
+        Args:
+            threshold (float, optional): The likelihood threshold below which interpolation should be performed.
+                                         Coordinates with a likelihood value lower than this threshold will be interpolated.
+                                         Defaults to 0.90.
+            
+            interpolation_method (str, optional): The method of interpolation to use. It can be any method supported
+                                                  by pandas.DataFrame.interpolate. Common methods include:
+                                                  - 'linear': Linear interpolation; default method that usually works well.
+                                                  - 'time': Interpolation over time (useful if your data is time-indexed).
+                                                  - 'index', 'values': Use the actual numerical values of the index.
+                                                  - 'pad', 'ffill': Forward fill.
+                                                  - 'bfill', 'backfill': Backward fill.
+                                                  - 'nearest': Nearest interpolation.
+                                                  More advanced methods like 'polynomial' and 'spline' require additional 
+                                                  parameters. Defaults to 'linear'.
+        """        
+        self.threshold = threshold
+        self.interpolation_method = interpolation_method
+
+    def get_interpolation(self, df, bodyparts):
+        """
+        Interpolate x,y coordinates for specified body parts in a single DataFrame.
+
+        Args:
+            df (pd.DataFrame): DataFrame of bodypart coordinates.
+            bodyparts (list): A list of bodyparts to interpolate.
+
+        Returns:
+            pd.DataFrame: DataFrame with interpolated values for the specified body parts.
+        """
+        scorer = df.columns.get_level_values('scorer')[0]  # Assuming the scorer is consistent
         for bp in bodyparts:
             if bp in df.columns.get_level_values('bodyparts').unique():
-            
-                mask = df.loc[:, (slice(None), bp, 'likelihood')] < threshold
+                mask = df.loc[:, (scorer, bp, 'likelihood')] < self.threshold
+                df.loc[mask, (scorer, bp, 'x')] = df[(scorer, bp, 'x')].interpolate(method=self.interpolation_method)
+                df.loc[mask, (scorer, bp, 'y')] = df[(scorer, bp, 'y')].interpolate(method=self.interpolation_method)
+            else:
+                print(f"{bp} bodypart is not found in the DataFrame.")
+        return df
 
-                df.loc[mask, (bp, 'x')] = df[bp]['x'].interpolate(method=interpolation_method)
-                df.loc[mask, (bp, 'y')] = df[bp]['y'].interpolate(method=interpolation_method)
+    def interpolate_data(self, input_data, bodyparts):
+        """
+        Interpolate data for either a single DataFrame or a dictionary of DataFrames.
 
-        data_dict[key] = df
-    return data_dict
+        Args:
+            input_data (str): Path to .h5 file/s.
+            bodyparts (list): List of bodyparts for interpolation.
+
+        Returns:
+            pd.DataFrame or dict: Interpolated DataFrame or dictionary of DataFrames.
+        """
+        if isinstance(input_data, str):
+            if os.path.isfile(input_data):
+                # If it's a file; read the single file
+                loader = DataLoader(h5_path=os.path.dirname(input_data))
+                df = loader.read_data(input_data)
+                return self.get_interpolation(df, bodyparts)
+            elif os.path.isdir(input_data):
+                # If it's a directory; read all files in the directory
+                loader = DataLoader(h5_path=input_data)
+                data_dict = loader.read_all_data()
+                return {key: self.get_interpolation(df, bodyparts) for key, df in data_dict.items()}
+            else:
+                raise ValueError("Provided path does not exist.")
+
+        else:
+            raise ValueError("Input data must be a file path or directory path")
+
 
 
 
--- dlc/plotting.py
+++ dlc/plotting.py
@@ -3,82 +3,197 @@ from matplotlib.collections import LineCollection
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+from pathlib import Path
 
-def interpolation_plot(ax, data, title, style='light'):
-    """
-    Plot the provided data on a given axis, highlighting segments based on likelihood values.
+from dlc.data import Interpolation
+from dlc.data import Centroid
+from dlc.load_data import DataLoader
 
-    This function visualizes animal tracking data as a line plot where each segment of the line 
-    is color-coded based on likelihood values. The segments with a higher likelihood value will 
-    appear more prominently (i.e., greener in the RdYlGn colormap) than those with lower likelihood values.
+class InterpolationPlot:
+    def __init__(self, style='light', colormap='RdYlGn', linewidth=0.5, alpha=1):
+        """
+        Initialize an InterpolationPlot object for visualizing animal tracking data.
 
-    Args:
-        ax (matplotlib.axes.Axes): The matplotlib axes on which to plot the data.
-        data (pandas.DataFrame): A dataframe containing columns for 'x', 'y', and 'likelihood'. 
-                                 Each row represents a point in the tracking data.
-        title (str): Title to set for the provided axes.
-        style (str): Plot style. Default "light", but user can set it to "dark"
+        Args:
+            style (str, optional): Specifies the plot style ('light' or 'dark'). Defaults to 'light'.
+            colormap (matplotlib.colors.Colormap, optional): Colormap to be used for plotting. Defaults to plt.cm.RdYlGn.
+            linewidth (float, optional): Width of the plot lines. Defaults to 0.5.
+            alpha (float, optional): Transparency level of the plot lines. Defaults to 0.5.
+        """
+        self.style = style
+        self.interpolator = Interpolation()
+        self.colormap = plt.get_cmap(colormap)
+        self.linewidth = linewidth
+        self.alpha = alpha
 
-    Returns:
-        matplotlib.collections.LineCollection: The LineCollection object that was added to the axes.
-    """
-    # Save the current style
-    current_style = plt.rcParams.copy()
+    def interpolation_plot(self, ax, data, title, cbar = True):
+        """
+        Plot data on a given axes object with specified style, colormap, linewidth, and alpha.
 
-    # Apply the desired style
-    if style == 'dark':
-        plt.style.use('dark_background')
-    else:
-        plt.style.use('default')
-    
-    try:
-        x = data.loc[:, (slice(None), slice(None), 'x')].values
-        y = data.loc[:, (slice(None), slice(None), 'y')].values
-        likelihood = data.loc[:, (slice(None), slice(None), 'likelihood')].values.flatten()
+        This function plots data on the provided axes with customizable styling parameters. It creates a line
+        plot connecting points in the 'x' and 'y' columns of the DataFrame. The color and transparency of the
+        lines are determined by the 'likelihood' column of the DataFrame. A colorbar is added to the plot to
+        indicate the likelihood values.
 
-        points = np.array([x, y]).T.reshape(-1, 1, 2)
-        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+        Args:
+            ax (matplotlib.axes.Axes): Matplotlib axes object to plot on.
+            data (pandas.DataFrame): DataFrame containing 'x', 'y', and 'likelihood' values.
+            title (str): Title of the plot.
 
-        colormap = plt.cm.RdYlGn
-        lc = LineCollection(segments, cmap=colormap, norm=plt.Normalize(likelihood.min(), likelihood.max()), linewidth=0.5, alpha=0.5)
-        lc.set_array(likelihood)
+        Returns:
+            matplotlib.collections.LineCollection: The LineCollection object representing the plotted data.
+        """        
+        current_style = plt.rcParams.copy()  # Save the current style
+        plt.style.use('dark_background' if self.style == 'dark' else 'default')
 
-        ax.add_collection(lc)
-        ax.set_xlim(x.min(), x.max())
-        ax.set_ylim(y.min(), y.max())
-        ax.set_title(title)
+        try:
+            x = data['x'].values
+            y = data['y'].values
+            likelihood = data['likelihood'].values
 
-        return lc
-    finally:
-        # Restore the original style
-        plt.rcParams.update(current_style)
+            points = np.array([x, y]).T.reshape(-1, 1, 2)
+            segments = np.concatenate([points[:-1], points[1:]], axis=1)
 
-def density_plot(bodypart, cmap='viridis', cbar=True, title="", style='light'):
-    """
-    Generate a 2D kernel density estimate (KDE) plot for the provided data points.
+            norm = plt.Normalize(likelihood.min(), likelihood.max())
+                
+            lc = LineCollection(segments, cmap=self.colormap, norm=norm, array=likelihood, linewidth=self.linewidth, alpha=self.alpha)
+            lc.set_array(likelihood)
 
-    Args:
-        bodypart (pandas.DataFrame): A dataframe containing columns 'x' and 'y'.
-        cmap (str, optional): Colormap for the KDE plot. Defaults to 'viridis'.
-        cbar (bool, optional): Whether to display a colorbar. Defaults to True.
-        title (str, optional): Title for the plot. Defaults to an empty string.
-        style (str): Plot style. Default "light", user can set to "dark".
+            ax.add_collection(lc)
+            ax.autoscale()
 
-    Returns:
-        None: Saves the plot as a PDF file.
-    """ 
-    if style == 'dark': 
-        plt.style.use('dark_background') 
-    elif style == "light":
-        plt.style.use('default') 
+            # Adding colorbar
+            if cbar:
+                cbar = plt.colorbar(lc, ax=ax)
+                cbar.set_label('Likelihood Value')
 
-    plt.subplots(figsize=(10, 10))
-    sns.set(style="dark")
 
-    x_data = bodypart.loc[:,(slice(None), slice(None), 'x')].values.flatten()
-    y_data = bodypart.loc[:,(slice(None), slice(None), 'y')].values.flatten()
+            return lc
+        finally:
+            plt.rcParams.update(current_style)
 
-    ax = sns.kdeplot(
+    def plot_bodyparts(self, file_path, bodyparts, title, save=False, save_directory='.', save_filename='plot.png'):
+        """
+        Plot data for multiple body parts from a single file.
+
+        This function reads data from a single file specified by 'file_path' and plots the trajectories of
+        multiple body parts over time. The 'bodyparts' argument should be a list of body part names to be
+        included in the plot. Each body part will be plotted in a separate subplot.
+
+        Args:
+            file_path (str): Path to the .h5 file containing the data.
+            bodyparts (list of str): List of body parts to include in the plot.
+            title_base (str): Base title for the plots, each subplot will have this title followed by the body part name.
+            save (bool, optional): If True, save the plot to the specified directory. If False, display the plot. Defaults to False.
+
+        """        
+        data = self.interpolator.interpolate_data(file_path, bodyparts)
+        num_bodyparts = len(bodyparts)
+        fig, axes = plt.subplots(1, num_bodyparts, figsize=(5 * num_bodyparts, 5), squeeze=False)
+
+        for i, bp in enumerate(bodyparts):
+            ax = axes[0, i]
+            title = f"{title} - {bp}"
+
+            scorer = data.columns.levels[0][0]
+            # Constructing a DataFrame for each body part
+            bp_data = pd.DataFrame({
+                'x': data[(scorer, bp, 'x')].values,
+                'y': data[(scorer, bp, 'y')].values,
+                'likelihood': data[(scorer, bp, 'likelihood')].values
+            })
+
+            self.interpolation_plot(ax, bp_data, title)
+
+        plt.tight_layout()
+        if save:
+            save_path = Path(save_directory) / save_filename
+            Path(save_directory).mkdir(parents=True, exist_ok=True)  # Create directory if it does not exist
+            plt.savefig(save_path)
+        else:
+            plt.show()
+
+    def plot_file(self, file_path, bodyparts, title_base):
+        """
+        Creates a figure with subplots for each body part from a single file.
+
+        Args:
+            file_path (str): Path to the .h5 file.
+            bodyparts (list of str): Body parts to include in the plot.
+            title_base (str): Base title for the plots, each subplot will have this title followed by the body part name.
+        """
+        self.plot_bodyparts(file_path, bodyparts, title_base)
+
+    def plot_directory(self, directory_path, bodyparts, title_base):
+        """
+        Creates figures with subplots for each body part from all .h5 files in a directory.
+
+        Args:
+            directory_path (str): Path to the directory containing .h5 files.
+            bodyparts (list of str): Body parts to include in the plot.
+            title_base (str): Base title for the plots, each subplot will have this title followed by the body part name.
+        """
+        directory = Path(directory_path)
+        h5_files = list(directory.glob('*filtered.h5'))  # Adjust pattern if needed
+
+        for file_path in h5_files:
+            self.plot_file(str(file_path), bodyparts, title_base.format(file_path.stem))
+
+    def plot_comparison(self, file_path, bodyparts, title):
+        """
+        Compares data before and after interpolation in side-by-side plots.
+
+        Args:
+            file_path (str): Path to the .h5 file.
+            bodyparts (list of str): Body parts to include in the plot.
+            title (str): Title of the plot.
+            colormap, linewidth, alpha: Styling parameters for the plot.
+        """
+        loader = DataLoader(file_path)
+        raw_data = loader.read_data(file_path)  
+        print("File path:", file_path)
+
+        int_data = self.interpolator.interpolate_data(input_data=file_path, bodyparts=bodyparts)
+
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+        self.plot_bodyparts(ax1, raw_data, f"{title} - Before Interpolation")
+        self.plot_bodyparts(ax2, int_data, f"{title} - After Interpolation")
+
+        # Add colorbar and show plot
+        cbar_ax = fig.add_axes([0.92, 0.15, 0.02, 0.7])
+        cbar = fig.colorbar(ax2.collections[0], cax=cbar_ax)  # Assuming ax2 has the LineCollection
+        cbar.set_label('Likelihood Value')
+        plt.tight_layout()
+        plt.show()
+
+class TrackingPlot:
+    def __init__(self, style='light'):
+        self.style = style
+        if self.style == 'dark':
+            plt.style.use('dark_background')
+        elif self.style == 'light':
+            plt.style.use('default')
+
+    def density_plot(self, bodypart, cmap='viridis', cbar=True, title=""):
+        """
+        Generate a 2D kernel density estimate (KDE) plot for the provided data points.
+
+        Args:
+            bodypart (pandas.DataFrame): A dataframe containing columns 'x' and 'y'.
+            cmap (str, optional): Colormap for the KDE plot. Defaults to 'viridis'.
+            cbar (bool, optional): Whether to display a colorbar. Defaults to True.
+            title (str, optional): Title for the plot. Defaults to an empty string.
+
+        Returns:
+            None: Saves the plot as a PDF file.
+        """
+        plt.figure(figsize=(10, 10))
+        sns.set(style=self.style)
+
+        x_data = bodypart.loc[:, (slice(None), slice(None), 'x')].values.flatten()
+        y_data = bodypart.loc[:, (slice(None), slice(None), 'y')].values.flatten()
+
+        ax = sns.kdeplot(
             x=x_data,
             y=y_data,
             fill=True,
@@ -86,8 +201,49 @@ def density_plot(bodypart, cmap='viridis', cbar=True, title="", style='light'):
             cmap=cmap,
             levels=10,
             alpha=0.9,
-        )    
+        )
 
-    plt.title(title)
-    ax.set_aspect("equal", adjustable="box") 
-    plt.savefig("Density_plot_" + title + ".pdf", format="pdf", dpi=100)
+        plt.title(title)
+        ax.set_aspect("equal", adjustable="box")
+        plt.savefig("Density_plot_" + title + ".pdf", format="pdf", dpi=100)
+
+    def line_plot(self):
+        pass  # Implement the line plot function here
+
+    def plot_file(self, file_path, bodyparts, title):
+        """
+        Plot data from a single file.
+
+        Args:
+            file_path (str): Path to the .h5 file.
+            bodyparts (list): List of bodyparts to include in the plot.
+            title (str): Title of the plot.
+
+        Returns:
+            None: Saves the plot as a PDF file.
+        """
+        # Load data from file (you may need to adapt this part)
+        data = pd.read_hdf(file_path)
+
+        # Perform necessary operations on data and call plot functions
+        # Example: self.density_plot(data, cmap='viridis', cbar=True, title=title)
+
+    def plot_directory(self, directory_path, bodyparts, title):
+        """
+        Plot data from all files in a directory.
+
+        Args:
+            directory_path (str): Path to the directory containing .h5 files.
+            bodyparts (list): List of bodyparts to include in the plot.
+            title (str): Title of the plot.
+
+        Returns:
+            None: Saves the plots for all files in the directory.
+        """
+        # List all .h5 files in the directory
+        h5_files = [f for f in os.listdir(directory_path) if f.endswith(".h5")]
+
+        for file in h5_files:
+            file_path = os.path.join(directory_path, file)
+            self.plot_file(file_path, bodyparts, title + "_" + os.path.splitext(file)[0])
+
