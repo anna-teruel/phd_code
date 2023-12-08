4cdcdf3323f7a6c0f0caacce106f60870fcf7ca0
anna-teruel <teruel.anna@gmail.com>
2023-12-09 00:15:51 +0100

    improved load_data and data file. work in progress with plotting.py

--- dlc/load_data.py
+++ dlc/load_data.py
@@ -1,63 +1,64 @@
 import os
-import os.path
 import pandas as pd
 import numpy as np
 
-def get_h5List(h5_path):
-    """
-    This code is used to get list of .h5 files to be processed within a directory
+class DataLoader:
+    def __init__(self, h5_path=None, minutes=None, fps=None):
+        """
+        Initialize the data loader with the path to the .h5 files, the number of minutes to read, and the fps.
 
-    Args:
-        h5_path (str): path to .h5 files to be processed
+        Args:
+            h5_path (str): Path to .h5 files to be processed.
+            minutes (int): Number of minutes of data to be read from each file.
+            fps (int): Frames per second of the data in the files.
+        """
+        self.h5_path = h5_path
+        self.minutes = minutes
+        self.fps = fps
 
-    Returns:
-        h5List (list): list of paths of .h5 files to be processed
-    """    
-    h5_path = os.chdir(h5_path)
-    h5List = []
-    for dirpath, dirnames, filenames in os.walk("."):
-        for filename in [f for f in filenames if f.endswith("filtered.h5")]:
-            h5List.append(os.path.join(dirpath, filename))
-    h5List.sort()
-    return h5List
+    def read_data(self, h5_file):
+        """
+        Reads data from a specified .h5 file for a given duration.
 
-def read_data(h5_file, minutes, fps):
-    """
-    Reads data from a specified .h5 file for a given duration.
+        Args:
+            h5_file (str): Path to the .h5 file to be read.
 
-    Args:
-        h5_file (str): Path to the .h5 file to be read.
-        minutes (int): Number of minutes of data to be read from the file.
-        fps (int): Frames per second of the data in the file.
+        Returns:
+            pd.DataFrame: A pandas DataFrame containing the specified duration of data from the .h5 file.
+        """
+        df = pd.read_hdf(h5_file)
+        
+        if self.minutes is not None and self.fps is not None:
+            frames = self.fps * 60 * self.minutes  
+            df = pd.read_hdf(h5_file)[:frames]
+        return df
 
-    Returns:
-        pd.DataFrame: A pandas DataFrame containing the specified duration of data from the .h5 file.
-    """   
-    df = pd.read_hdf(h5_file)
+    def get_h5_list(self):
+        """
+        Get a list of .h5 files to be processed within a directory.
 
-    frames = fps * 60 * minutes  
-    df = df[:frames]
-    return df
+        Returns:
+            h5List (list): List of paths of .h5 files to be processed.
+        """
+        h5_list = []
+        for dirpath, dirnames, filenames in os.walk(self.h5_path):
+            for filename in [f for f in filenames if f.endswith("filtered.h5")]:
+                h5_list.append(os.path.join(dirpath, filename))
+        h5_list.sort()
+        return h5_list
 
-def read_all_data(h5_path, minutes, fps):
-    """
-    Reads data from all .h5 files in the provided path for batch processing
+    def read_all_data(self):
+        """
+        Reads data from all .h5 files in the provided path for batch processing.
 
-    Args:
-        h5_path (str): Path to .h5 files to be processed.
-        minutes (int): Number of minutes to be read from each file.
-        fps (int): Frames per second of the data.
-
-    Returns:
-        dict: A dictionary with file names as keys and dataframes as values.
-    """
-    h5_list = get_h5List(h5_path)
-    data_dict = {}
-
-    for h5_file in h5_list:
-        df = read_data(h5_file, minutes, fps)
-        file_name = os.path.basename(h5_file)
-        data_dict[file_name] = df
-
-    return data_dict
+        Returns:
+            dict: A dictionary with file names as keys and dataframes as values.
+        """
+        h5_list = self.get_h5_list()
+        data_dict = {}
+        for h5_file in h5_list:
+            df = self.read_data(h5_file)
+            file_name = os.path.basename(h5_file)
+            data_dict[file_name] = df
+        return data_dict
 
