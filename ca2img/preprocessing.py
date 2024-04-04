5b9ba3e072f905fc4ddf0eca7680b7f40bd65bfc
anna-teruel <teruel.anna@gmail.com>
2024-04-04 12:02:17 +0200

    last edits on calcium imaging videos preprocessing

--- ca2img/preprocessing.py
+++ ca2img/preprocessing.py
@@ -19,26 +19,22 @@ def process_video(input_video, output_dir):
     Raises:
         ValueError: Error opening video file
     """
-    # Ensure output directory exists
-    if not os.path.isdir(output_dir):
-        os.makedirs(output_dir, exist_ok=True)
-
-    # Load the video
     cap = cv2.VideoCapture(input_video)
     if not cap.isOpened():
         raise ValueError("Error opening video file")
     
-    # Video properties
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     
-    # Prepare to write the processed video
     filename = os.path.basename(input_video)
     processed_filename = f"p_{filename}"
-    output_video_path = os.path.join(output_dir, processed_filename)  # Correct full path including filename
+    video_path = os.path.join(output_dir, processed_filename)  # Correct full path including filename
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
-    out = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))  # Use the full path here
+    out = cv2.VideoWriter(video_path, fourcc, fps, (width, height))  
+
+    if not out.isOpened():
+        raise ValueError(f"Could not open the video writer for the file {video_path}")
 
     frames = []
     ret, frame = cap.read()
@@ -47,17 +43,18 @@ def process_video(input_video, output_dir):
         ret, frame = cap.read()
 
     cap.release()
+    if not frames:
+        raise ValueError(f"No frames to process in video: {input_video}")
     frames = np.array(frames)
 
     min_intensity_proj = np.min(frames, axis=0)
     subtracted_frames = frames - min_intensity_proj
 
-    # Write the processed frames to the video file
     for frame in subtracted_frames:
         out.write(frame.astype(np.uint8))
 
     out.release()
-    print(f"Processed video saved as {output_video_path}")
+    print(f"Processed video saved as {video_path}")
 
 
 def process_directory(root_dir):
@@ -66,21 +63,20 @@ def process_directory(root_dir):
 
     Args:
         root_dir (str): root directory containing the videos to process
-    """    
+    """
     for root, dirs, files in os.walk(root_dir):
         for file in files:
             if file.endswith('.avi') and not file.startswith('p_'):
-                input_video_path = os.path.join(root, file)
-                output_video_path = os.path.join(root, f"p_{file}")
-                process_video(input_video_path, output_video_path)
+                input_path = os.path.join(root, file)
+                process_video(input_path, root)
 
 def convert_to_mjpeg(input_video, output_dir):
     """
     Convert a video file to MJPEG AVI format using FFmpeg.
 
     Args:
-        input_video_path (str): The path to the input video file.
-        output_video_path (str): The path where the output video will be saved.
+        input_video (str): The path to the input video file.
+        output_dir (str): The path where the output video will be saved.
     """
     command = [
         'ffmpeg',
