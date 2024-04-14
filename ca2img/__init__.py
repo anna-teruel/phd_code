8aa5f0497c47b6e4628f43d052cb4b9760ad60cc
anna-teruel <teruel.anna@gmail.com>
2024-04-14 21:18:54 +0200

    automation of minian's pre-processing

--- ca2img/__init__.py
+++ ca2img/__init__.py
@@ -3,6 +3,7 @@ This code is written to run calcium imaging analysis
 """
 #init file
 
-import minian.preprocessing as preprocessing
+import ca2img.preprocessing as preprocessing
+import ca2img.minian_dca1 as miniandca1
 
-__all__ = ["preprocessing"]
+__all__ = ["preprocessing", "minian_dca1"]
