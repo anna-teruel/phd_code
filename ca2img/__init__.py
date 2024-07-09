7f8658bbdae3f2e7410bcb2333de53cb43683337
anna-teruel <teruel.anna@gmail.com>
2024-07-09 17:08:24 +0200

    trying to solve merge conflicts

--- ca2img/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This code is written to run calcium imaging analysis
-"""
-#init file
-
-import ca2img.preprocessing as preprocessing
-import ca2img.minian_dca1 as miniandca1
-
-__all__ = ["preprocessing", "minian_dca1"]
